ea16908230799239ea0b81a85dc7f9a99ac17e9e
brian <toscano4@msu.edu>
2024-04-21 21:49:38 -0400

    dwadw

--- weatherdata.js
+++ weatherdata.js
@@ -1,6 +1,6 @@
 
 const supabaseUrl = 'https://zlldvwdafeytxvorhtqo.supabase.co'; 
-const supabaseKey = 'YOUR_SUPABASE_KEY'; 
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpsbGR2d2RhZmV5dHh2b3JodHFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM3MjY2MTUsImV4cCI6MjAyOTMwMjYxNX0.7So7AXX8oFNwtDIkob3QQjwrlWIX6loBqgIIWouj1JI'; 
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
 async function fetchData() {
